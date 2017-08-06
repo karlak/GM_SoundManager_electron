71ceaca3670aaba5566f421002db8b69ac1bc4d7
Bocquet Aldric <aldricbocquet@gmail.com>
2017-08-07 01:17:24 +0200

    First UI tests + restoring previous window state at launch

--- main.js
+++ main.js
@@ -1,55 +1,48 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-
+const windowStateKeeper = require('electron-window-state');
+const gm_music = require('./gm_music_lib/bin/win32-x64-53/gm_music_lib.node');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600
+  });
 
-  // and load the index.html of the app.
+  win = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    'frame': true
+  });
+  mainWindowState.manage(win);
+
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
   win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+app.on('window-all-closed', app.quit)
+app.on('activate', () => {})
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+/*const ipc = require('electron').ipcMain
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+ipc.on('synchronous-message', function (event, arg) {
+  event.returnValue = gm_music;
+  console.log('ping received!');
+})*/
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
