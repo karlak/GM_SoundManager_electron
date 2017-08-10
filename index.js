c21d76c2060c6c5b3027ff3d87eb18ebd43d53a4
Bocquet Aldric <aldricbocquet@gmail.com>
2017-08-10 18:05:06 +0200

    js styling update

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const ipc = require('electron').ipcRenderer
 const { remote } = require('electron')
+const win = remote.getCurrentWindow();
 
-let win = remote.getCurrentWindow();
+// Prevents the middle click scroll behavior
+document.body.onmousedown = e => { if (e.button === 1) return false; };
 
 function check_position() {
     var pos = win.getPosition();
     if(pos[0]==-4000&&pos[1]==-4000){win.center();}
 }
-check_position();
-
-ipc.on('maximize', (event, message) => {
-  document.getElementById("titlebar").classList.add('maximized');
-  syncMsgBtn.value= 'maximized';
-})
-ipc.on('unmaximize', (event, message) => {
-  document.getElementById("titlebar").classList.remove('maximized');
-  syncMsgBtn.value= 'unmaximized';
-})
 
 function minimize() {
     win.minimize();
 }
+
 function maximize() {
     if(win.isMaximized()){
         win.unmaximize();
@@ -31,53 +24,70 @@ function maximize() {
     }
 
 }
+
 function closewindow() {
     win.close();
 }
 
-//
 function update_cpuusage(argument) {
     cpu = remote.process.getCPUUsage();
     document.getElementById("cpuusage").innerHTML = "[CPU "+cpu.percentCPUUsage.toFixed(2) + "%]";
 }
-var t=setInterval(update_cpuusage,1000);
 
-/******* Splitter ******/
-const element_split_parent = document.querySelector("#split-parent");
+///////////////////////////////////////
+///////////////////////////////////////
 
-Split(['#left-panel', '#right-panel'], {
-    gutterSize: 5,
-    snapOffset: 0,
-    elementStyle: function (dimension, size, gutterSize, num=-1) {
-            // console.log(num);
-        if (typeof size === 'string' || size instanceof String) {
-            if (size.indexOf('%')>0 || size.indexOf('px')>0) {
-                var ret = [];
-                ret['flex-basis'] = 'calc(' + size + ' - ' + gutterSize + 'px)';
-                return ret;
+window.addEventListener("load", () => {
+    check_position();
+    
+    ipc.on('maximize', (event, message) => {
+        document.getElementById("titlebar").classList.add('maximized');
+        syncMsgBtn.value= 'maximized';
+    })
+    ipc.on('unmaximize', (event, message) => {
+        document.getElementById("titlebar").classList.remove('maximized');
+        syncMsgBtn.value= 'unmaximized';
+    })
+    var t=setInterval(update_cpuusage,1000);
+    
+    /******* Splitter ******/
+    const element_split_parent = document.querySelector("#split-parent");
+
+    Split(['#left-panel', '#right-panel'], {
+        gutterSize: 5,
+        snapOffset: 0,
+        elementStyle: function (dimension, size, gutterSize, num=-1) {
+                // console.log(num);
+                if (typeof size === 'string' || size instanceof String) {
+                    if (size.indexOf('%')>0 || size.indexOf('px')>0) {
+                        var ret = [];
+                        ret['flex-basis'] = 'calc(' + size + ' - ' + gutterSize + 'px)';
+                        return ret;
+                    }
+                    return {};
+                }
+
+                if(num==0){
+                    var calc_size_px = element_split_parent.offsetWidth * (size / 100.0);
+                    var ret = [];
+                    ret['flex-basis'] = 'calc(' + calc_size_px + 'px - ' + gutterSize + 'px)';
+                    return ret;
+                }
+                return {};
+            },
+            gutterStyle: function (dimension, gutterSize) {
+                return {
+                    'flex-basis':  gutterSize + 'px',
+                    'min-width': gutterSize + 'px'
+                }
             }
-            return {};
-        }
+        })
+    /***********************/
 
-        if(num==0){
-            var calc_size_px = element_split_parent.offsetWidth * (size / 100.0);
-            var ret = [];
-            ret['flex-basis'] = 'calc(' + calc_size_px + 'px - ' + gutterSize + 'px)';
-            return ret;
-        }
-        return {};
-    },
-    gutterStyle: function (dimension, gutterSize) {
-        return {
-            'flex-basis':  gutterSize + 'px',
-            'min-width': gutterSize + 'px'
-        }
-    }
-})
-/***********************/
+});
 
-// Prevents the middle click scroll behavior
-document.body.onmousedown = e => { if (e.button === 1) return false; };
+
+
 
 
 
