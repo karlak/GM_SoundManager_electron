f5e20d252ce6a9e9bb49ca08fad502578858d8a4
Bocquet Aldric <aldricbocquet@gmail.com>
2017-08-11 00:42:56 +0200

    Tree loaded from database + drag'n'drop visually work
    Added Jquery-ui to the project

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const win = remote.getCurrentWindow();
 const Datastore = remote.require('nedb')
 const path = require('path');
 var db;
-    
+
 // Prevents the middle click scroll behavior
 document.body.onmousedown = e => { if (e.button === 1) return false; };
 
@@ -49,11 +49,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     ipc.on('maximize', (event, message) => {
         document.getElementById("titlebar").classList.add('maximized');
-        syncMsgBtn.value= 'maximized';
     })
     ipc.on('unmaximize', (event, message) => {
         document.getElementById("titlebar").classList.remove('maximized');
-        syncMsgBtn.value= 'unmaximized';
     })
     var t=setInterval(update_cpuusage,1000);
     
@@ -100,19 +98,84 @@ document.addEventListener("DOMContentLoaded", () => {
     /********Database********/
     let db_filename = path.join(app.getPath('userData'), 'something.db');
     db = new Datastore({ filename: db_filename, autoload: true});
-    // db.insert({hello:'world!'});
+    //db.insert({hello:'world!'});
     // db.persistence.compactDatafile();
-    db.find({}, function (err, docs) {
-        console.log(docs);
-    });
+    // db.find({}, function (err, docs) {
+    //     console.log(docs);
+    // });
 
 
     /************************/
     /********TreeView********/
-    $(function(){
-      $("#tree").fancytree();
-    });
+    function myLazyLoad(event, data) {
+        var node = data.node;
+        var parent_key = node.key;
+        console.log("Lazy load for key: ", parent_key);
 
+        
+        var dfd = new $.Deferred();
+        data.result = dfd.promise();
+        
+        db.find({ parent: parent_key }, function (err, docs) {
+            var loaded = docs.map((doc)=>{
+                return { title: doc.title, folder: doc.is_folder, key: doc._id, lazy: doc.is_folder};
+            })
+            console.log("Loaded: ", loaded);
+            dfd.resolve(loaded);
+        });
+    }
+    $("#tree").fancytree({
+        source: [
+        {title: "Library", key: "root", folder: true, lazy: true},
+        ],
+        lazyLoad: myLazyLoad,
+        extensions: ["dnd"],
+        dnd: {
+            // Available options with their default:
+            autoExpandMS: 400,          // Expand nodes after n milliseconds of hovering
+            draggable: {                // modify default jQuery draggable options
+                zIndex: 1000,
+                scroll: true,
+                containment: "parent",
+                revert: "invalid"
+            },
+            droppable: null,            // Additional options passed to jQuery UI droppable
+            dropMarkerOffsetX: -24,     // absolute position offset for .fancytree-drop-marker
+                                        // relatively to ..fancytree-title (icon/img near a node accepting drop)
+            dropMarkerInsertOffsetX: -16, // additional offset for drop-marker with hitMode = "before"/"after"
+            focusOnClick: false,        // Focus, although draggable cancels mousedown event (#270)
+            preventRecursiveMoves: true,// Prevent dropping nodes on own descendants
+            preventVoidMoves: true,     // Prevent dropping nodes 'before self', etc.
+            smartRevert: true,          // set draggable.revert = true if drop was rejected
+
+            // Events that make tree nodes draggable
+            dragStart: function(node, data) {
+                if(node.key == "root") {return false;}
+                return true;
+            },
+            dragStop:     (sourceNode, data)=>{return true},
+            initHelper:   (sourceNode, data)=>{return false},
+            updateHelper: (sourceNode, data)=>{return false},
+
+            // Events that make tree nodes accept draggables
+            dragEnter: (targetNode, data)=> {
+                /* Return 'over', 'before, or 
+                'after' to force a hitMode.*/
+                return 'over';
+            },            
+            dragExpand: (targetNode, data)=> {
+                //return false to prevent autoExpand
+                if(data.otherNode != null && data.otherNode.key == targetNode.key)
+                    return false;
+                return true
+            },           
+            dragOver: (targetNode, data)=>{return true},
+            dragDrop: (targetNode, data)=>{
+                data.otherNode.moveTo(targetNode, data.hitMode);
+            },
+            dragLeave: (targetNode, data)=>{return true}
+        },
+    });
 
 
     /************************/
